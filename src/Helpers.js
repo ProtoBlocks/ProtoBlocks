b9cc17854d138816b612f0fa0f8bef63779d813d
Vivek Nair <vivek@nair.me>
2021-11-27 15:17:13 -0800

    more helper functions

--- src/Helpers.js
+++ src/Helpers.js
@@ -6,3 +6,42 @@ module.exports.hash = (str) => {
 }
 
 module.exports.nonce = uuid
+
+module.exports.timestamp = (granularity = 0) => {
+  const time = Date.now()
+  return (time - (time % granularity)).toString()
+}
+
+module.exports.encrypt = (plaintext, key) => {
+  const cipher = crypto.createCipheriv('aes-128-cbc', key, 'BDA30EGDH1578F81')
+  let crypted = cipher.update(plaintext, 'ascii', 'base64')
+  crypted += cipher.final('base64')
+  return crypted
+}
+
+module.exports.decrypt = (ciphertext, key) => {
+  try {
+    const decipher = crypto.createDecipheriv('aes-128-cbc', key, 'BDA30EGDH1578F81')
+    let decoded = decipher.update(ciphertext, 'base64', 'ascii')
+    decoded += decipher.final('ascii')
+    return decoded
+  } catch (e) {
+    return false
+  }
+}
+
+module.exports.keygen = () => {
+  return crypto.randomBytes(8).toString('hex')
+}
+
+module.exports.keypairgen = () => {
+  return crypto.generateKeyPairSync('rsa', { modulusLength: 2048 })
+}
+
+module.exports.sign = (message, key) => {
+  return crypto.sign('SHA256', Buffer.from(message), key).toString('base64')
+}
+
+module.exports.verify = (message, signature, key) => {
+  return crypto.verify('SHA256', Buffer.from(message), key, Buffer.from(signature, 'base64'))
+}
