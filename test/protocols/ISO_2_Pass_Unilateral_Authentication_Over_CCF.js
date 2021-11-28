61ddc521f54a58a87488fdd434bf25a1fb7794dc
Vivek Nair <vivek@nair.me>
2021-11-28 09:44:22 -0800

    cleanup

--- test/protocols/ISO_2_Pass_Unilateral_Authentication_Over_CCF.js
+++ test/protocols/ISO_2_Pass_Unilateral_Authentication_Over_CCF.js
@@ -3,42 +3,17 @@ const { Protocol, hash, nonce } = require('../../src')
 /* eslint camelcase: "off" */
 const ISO_2_Pass_Unilateral_Authentication_Over_CCF = new Protocol({
   name: 'ISO_2_Pass_Unilateral_Authentication_Over_CCF',
-  principals: [{
-    name: 'Prover',
-    inputs: ['Secret']
-  }, {
-    name: 'Verifier',
-    inputs: ['Secret']
-  }],
-  steps: [{
-    origin: 'Verifier',
-    recipients: ['Prover'],
-    name: 'Challenge',
-    function: async (Prover, Verifier) => {
-      const Nonce = nonce()
-      Prover.send({
-        Nonce: Nonce
-      })
-    }
-  }, {
-    origin: 'Prover',
-    recipients: ['Verifier'],
-    name: 'Response',
-    function: async (Prover, Verifier) => {
-      const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Prover.Input.Secret)
-      Verifier.send({
-        Hash: Hash
-      })
-    }
-  }, {
-    origin: 'Verifier',
-    recipients: [],
-    name: 'Verify',
-    function: async (Prover, Verifier) => {
-      const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Verifier.Input.Secret)
-      return Hash === Prover.Response.Hash
-    }
-  }]
+  principals: [{name: 'Prover', inputs: ['Secret']}, {name: 'Verifier', inputs: ['Secret']}],
+  steps: [{origin: 'Verifier', recipients: ['Prover'], name: 'Challenge', function: async (Prover, Verifier) => {
+    const Nonce = nonce()
+    Prover.send({Nonce: Nonce})
+  }}, {origin: 'Prover', recipients: ['Verifier'], name: 'Response', function: async (Prover, Verifier) => {
+    const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Prover.Input.Secret)
+    Verifier.send({Hash: Hash})
+  }}, {origin: 'Verifier', recipients: [], name: 'Verify', function: async (Prover, Verifier) => {
+    const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Verifier.Input.Secret)
+    return Hash === Prover.Response.Hash
+  }}]
 })
 
 module.exports = ISO_2_Pass_Unilateral_Authentication_Over_CCF
